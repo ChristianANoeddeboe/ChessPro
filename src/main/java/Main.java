fd721e0cb3fc22cacb4536fd2b7c9d87168be020
christian <s164150@student.dtu.dk>
2020-10-06 14:47:23 +0000

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 import com.github.bhlangonijr.chesslib.*;
 import com.github.bhlangonijr.chesslib.move.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,6 +16,19 @@ public class Main {
         board.doMove(new Move(Square.E2, Square.E4));
 
         //print the chessboard in a human-readable form
-        System.out.println(board.toString());
+        //System.out.println(board.toString());
+        System.out.println("colors(0)");
+        Scanner scanner = new Scanner(System.in);
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            String command = null;
+            try {
+                command = input.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (command != null) System.out.println(command);;
+
+        }
     }
 }
